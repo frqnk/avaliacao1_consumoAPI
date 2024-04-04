1f5f4e429a4ccee8b313463f72db5b8d844ed662
Frank <33494411+frqnk@users.noreply.github.com>
2024-04-04 20:40:32 -0300

    alterações desnecessárias de última hora

--- main.py
+++ main.py
@@ -29,11 +29,26 @@ class IsEven_API:
     def request(self, number):
         return requests.get(self.URL+str(number))
 
+    def supportedRange(self):
+        return self.tier['range']
+
+    def minValue(self):
+        return self.tier['range'][0]
+
+    def maxValue(self):
+        return self.tier['range'][-1]
+
+    def hasAd(self):
+        return self.tier['advertisements']
+
+    def allowNegativeNumbers(self):
+        return self.tier['negative_numbers']
+
 class User_number:
     parity = None
 
-    def __init__(self):
-        self.input = input('\nDê um número: ')
+    def __init__(self, input):
+        self.input = input
 
         try:
             self.as_float = float(self.input.replace(',', '.'))
@@ -69,19 +84,15 @@ def remove_ad(content):
     content['ad'] = None
     return content
 
-API = IsEven_API()
-
-print(f'\nBem-vindo ao verificador de paridade de números entre {API.tier["range"][0]} e {API.tier["range"][-1]}.')
-
-while True:
-    user_number = User_number()
+def main():
+    user_number = User_number(input('\nDê um número: '))
 
     try:
         response = API.request(user_number.as_int)
 
         response.raise_for_status()
 
-        if API.tier['advertisements']:
+        if API.hasAd():
             user_number.parity = ('ímpar', 'par')[remove_ad(response.json())['iseven']]
         else:
             user_number.parity = ('ímpar', 'par')[response.json()['iseven']]
@@ -95,11 +106,11 @@ while True:
         elif user_number.is_not_a_whole_number():
             print('Números não inteiros não possuem paridade.')
 
-        elif user_number.is_a_negative_number() and API.tier['negative_numbers'] == False: 
+        elif user_number.is_a_negative_number() and API.allowNegativeNumbers() == False: 
             print('Números negativos não são suportados pelo plano atual. Considere migrar de plano para melhor atender às suas necessidades.')
 
-        elif user_number.as_int not in API.tier['range']:
-            print(f'{user_number.input} não está no intervalo de {API.tier["range"][0]} a {API.tier["range"][-1]} do plano atual. Considere migrar de plano para melhor atender às suas necessidades.')
+        elif user_number.as_int not in API.supportedRange():
+            print(f'{user_number.input} não está no intervalo de {API.minValue()} a {API.maxValue()} do plano atual. Considere migrar de plano para melhor atender às suas necessidades.')
 
         elif response.status_code in range(400, 500):
             print('Algo deu errado e a culpa é minha. Lamento.')
@@ -110,4 +121,26 @@ while True:
         else:
             print('Algo dentre muitas coisas que podiam dar errado deu errado. Tente novamente. Se continuar dando errado, encontre o problema e o corrija antes de tentar novamente. Boa sorte.')
 
-    if input('\nContinuar? (S/n) ').strip().lower() in {'n', 'nao', 'não'}: exit()
+def askToContinue():
+    answer = input('\nContinuar? (S/n) ').strip().lower()
+
+    if answer in {'n', 'no', 'nao', 'não'}:
+        exit()
+    elif answer in {'', 'y', 'yes', 's', 'sim'}:
+        pass
+    else:
+        askToContinue()
+
+def main_loop():
+    main()
+    askToContinue()
+    main_loop()
+
+def tests():
+    pass
+
+API = IsEven_API()
+
+print(f'\nBem-vindo ao verificador de paridade de números entre {API.minValue()} e {API.maxValue()}.')
+
+main_loop()
