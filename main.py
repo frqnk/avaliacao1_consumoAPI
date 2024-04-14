47d04acffe8ef700b92c380e7b73207227847911
Frank <33494411+frqnk@users.noreply.github.com>
2024-04-14 19:31:16 -0300

    try-except reduzido

--- main.py
+++ main.py
@@ -75,37 +75,30 @@ def main():
 
     try:
         response = API.request(user_number.as_int)
-
         response.raise_for_status()
-
-        if API.hasAd:
-            user_number.parity = ('ímpar', 'par')[remove_ad(response.json())['iseven']]
-        else:
-            user_number.parity = ('ímpar', 'par')[response.json()['iseven']]
-
-        print(f'{user_number.input} é um número {user_number.parity}.')
-
     except:
         if user_number.is_not_a_number():
             print(f'"{user_number.input}" não é um número válido.')
-
         elif user_number.is_not_a_whole_number():
             print('Números não inteiros não possuem paridade.')
-
         elif user_number.is_a_negative_number() and not API.allowNegativeNumbers: 
             print('Números negativos não são suportados pelo plano atual. Considere migrar de plano para melhor atender às suas necessidades.')
-
         elif user_number.as_int not in API.supportedRange:
             print(f'{user_number.input} não está no intervalo de {API.minValue} a {API.maxValue} do plano atual. Considere migrar de plano para melhor atender às suas necessidades.')
-
         elif response.status_code in range(400, 500):
             print('Algo deu errado e a culpa é minha. Lamento.')
-
         elif response.status_code in range(500, 600):
             print('Problema no servidor. Tente novamente mais tarde.')
-
         else:
             print('Algo dentre muitas coisas que podiam dar errado deu errado. Tente novamente. Se continuar dando errado, encontre o problema e o corrija antes de tentar novamente. Boa sorte.')
+        return
+        
+    if API.hasAd:
+        user_number.parity = ('ímpar', 'par')[remove_ad(response.json())['iseven']]
+    else:
+        user_number.parity = ('ímpar', 'par')[response.json()['iseven']]
+
+    print(f'{user_number.input} é um número {user_number.parity}.')
 
 def askToContinue():
     answer = input('\nContinuar? (S/n) ').strip().lower()
